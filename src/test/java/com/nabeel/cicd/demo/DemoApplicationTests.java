5b9d8a9d75590e19369576da0d9066e89833408f
Nabeel M <86767576+NABEELM1998@users.noreply.github.com>
2024-03-03 20:08:41 +0530

    initial commit

--- src/test/java/com/nabeel/cicd/demo/DemoApplicationTests.java
+++ src/test/java/com/nabeel/cicd/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.nabeel.cicd.demo;
 
+import com.nabeel.cicd.demo.controller.DemoController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@InjectMocks
+	private DemoController demoController;
+
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void testWelcomeMessage(){
+		String inp = "Hi welcome to demo controller ,";
+		String inputMessage = "nabeel";
+		ResponseEntity<String> response =  demoController.getWelcomeMessage(inputMessage);
+		Assertions.assertEquals(inp+inputMessage,response.getBody());
+	}
+
 
 }
